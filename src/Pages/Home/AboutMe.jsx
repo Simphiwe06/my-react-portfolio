c6739f55e560d6d09215e031015bed74d573d947
Simphiwe06 <simphiwembuli06@gmail.com>
2024-02-14 20:44:56 +0200

    created basic structure, added content

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          As a dedicated software developer, I specialize in front-end and full-stack solutions, primarily using JavaScript for its versatility. Proficient in HTML, CSS, and frameworks like React.js and Redux, I create intuitive user experiences. With expertise in Node.js and Git, I ensure seamless integration and streamlined workflows in collaborative environments.
+          </p>
+          <p className="hero--section-description">
+          In addition to my technical prowess, I bring a collaborative spirit and problem-solving mindset to every project. Whether working independently or in a team, I thrive on challenges and am dedicated to delivering high-quality solutions. Let's collaborate and turn your ideas into reality.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Simphiwe Mbuli</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Software</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Welcome to my digital realm of coding adventures.
+            <br /> -where creativity meets functionality and innovation.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}

12401cffea28289778fb06273cc784f942be22c5
Simphiwe06 <simphiwembuli06@gmail.com>
2024-02-14 23:53:35 +0200

    fixed responsiveness

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -5,7 +5,7 @@ export default function ContactMe() {
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
         <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+        Feel free to reach out to me with any inquiries or collaboration opportunities!
         </p>
       </div>
       <form className="contact--form--container">
--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -9,7 +9,7 @@ export default function MyPortfolio() {
           <h2 className="section--heading">My Portfolio</h2>
         </div>
         <div>
-          <button className="btn btn-github">
+          <button className="btn btn-github"  >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="32"
@@ -24,7 +24,7 @@ export default function MyPortfolio() {
                 fill="currentColor"
               />
             </svg>
-            Visit My GitHub
+            Hop into my Github
           </button>
         </div>
       </div>
